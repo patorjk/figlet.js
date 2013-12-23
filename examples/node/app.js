408f8443547e651279b531e648c33198b34d43ff
Patrick Gillespie <patorjk@gmail.com>
2013-12-23 01:22:41 -0500

    Updated example

--- examples/node/app.js
+++ examples/node/app.js
@@ -1,4 +1,17 @@
+/*
+    Example
+*/
+
 var figlet = require('../../lib/node-figlet.js');
+
+/*
+    Once this has been run:
+    
+    npm install figlet
+
+    Use the below line instead of the above line
+*/
+// var figlet = require('figlet');
 
 figlet.loadFont('Standard', function(err) {
     if (err) {
