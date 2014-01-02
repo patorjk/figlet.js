b0498731d1e586283dc312c0f751564a7ffbdf93
Patrick Gillespie <patorjk@gmail.com>
2014-01-02 15:02:01 -0500

    Added tests and made minor tweaks

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+/*
+ * figlet
+ * 
+ *
+ * Copyright (c) 2013 Patrick Gillespie
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'lib/*.js',
+                '<%= nodeunit.tests %>',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp'],
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
+        },
+        
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ test/figlet_test.js
@@ -0,0 +1,75 @@
+
+
+'use strict';
+
+var figlet = require('../lib/node-figlet'),
+    grunt = require('grunt');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.figlet = {
+    setUp: function(done) {
+        // setup here if necessary
+        done();
+    },
+    standard: function(test) {
+        test.expect(1);
+        
+        figlet('FIGlet\nFONTS', {
+            font: 'Standard',
+            verticalLayout: 'fitted'
+        }, function(err, actual) {
+            console.log('reu');
+
+            var expected = grunt.file.read('test/expected/standard');
+            test.equal(actual, expected, 'Standard font with a vertical layout of "fitted".');
+
+            test.done();
+        });
+    },
+    graffiti: function(test) {
+        test.expect(1);
+
+        figlet.text('ABC.123', {
+            font: 'Graffiti',
+            horizontalLayout: 'fitted'
+        }, function(err, actual) {
+            var expected = grunt.file.read('test/expected/graffiti');
+            test.equal(actual, expected, 'Graffiti font with a horizontal layout of "fitted".');
+
+            test.done();
+        });
+    },
+    dancingFont: function(test) {
+        test.expect(1);
+
+        figlet.text('pizzapie', {
+            font: 'Dancing Font',
+            horizontalLayout: 'full'
+        }, function(err, actual) {
+
+            var expected = grunt.file.read('test/expected/dancingFont');
+            test.equal(actual, expected, 'Dancing Font with a horizontal layout of "full".');
+
+            test.done();
+        });
+    },
+};
