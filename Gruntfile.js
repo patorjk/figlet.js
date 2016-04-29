806483bccb30da226ff1ddb1d4f5842d554d1d44
Patrick Gillespie <patorjk@gmail.com>
2016-04-28 23:00:25 -0400

    Removed some comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,3 @@
-/*
- * figlet
- * 
- *
- * Copyright (c) 2013 Patrick Gillespie
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 module.exports = function(grunt) {
@@ -39,8 +31,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    //grunt.loadNpmTasks('grunt-contrib-uglify');
-
+    
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
     grunt.registerTask('test', ['clean', 'nodeunit']);
